67d2e5478a38528d1c57cd7c7586485b42babfab
olivernilssson <olivernilsson@live.se>
2024-02-23 17:28:51 +0100

    Implement header and other major changes

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen items-center justify-between p-24">
+      <h1 className="text-4xl font-bold text-center">Welcome to FinAPI</h1>
+    </main>
+  );
+}
