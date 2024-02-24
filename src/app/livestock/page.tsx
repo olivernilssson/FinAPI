3392e0bd5a850cd076fa77e9f976e2b6a4be6b60
olivernilssson <olivernilsson@live.se>
2024-02-24 13:57:06 +0100

    Update StockLive component to display ticker, market status and connection

--- src/app/livestock/page.tsx
+++ src/app/livestock/page.tsx
@@ -3,8 +3,8 @@ import { StockLive } from "@/lib/util/stock-live/stock-live"
 export default function LiveStock() {
     return (
         <div className="content-container w-full">
-            <div className="grid grid-cols-3 gap-4">
-                <StockLive />
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <StockLive ticker="AAPL" />
             </div>
         </div>
     )
--- src/lib/util/stock-live/stock-live.tsx
+++ src/lib/util/stock-live/stock-live.tsx
@@ -10,10 +10,13 @@ function formatPrice(price: number) {
     return `$${price.toFixed(2)}`;
 }
 
-export function StockLive() {
+export function StockLive({ ticker }: { ticker: string }) {
     const [prevStock, setPrevStock] = useState<Message<{}> | null | undefined>(null);
     const [currentStock, setCurrentStock] = useState<Message<{}> | null | undefined>(null);
     const [direction, setDirection] = useState('up');
+    const [connect, setConnect] = useState(false);
+    const [marketStatus, setMarketStatus] = useState('loading');
+
     useEffect(() => {
         const ws = new WebSocket('wss://streamer.finance.yahoo.com');
         protobuf.load('./YPricingData.proto', (error: Error | null, root: Root | undefined) => {
@@ -22,18 +25,33 @@ export function StockLive() {
                 return;
             }
 
+            let timeout: NodeJS.Timeout;
+            const setMarketClosed = () => {
+                timeout = setTimeout(() => {
+                    console.log('Market Closed');
+                    setMarketStatus('closed');
+                }, 3000);
+            }
+
+            setMarketClosed();
+
             const Yaticker = root?.lookupType('yaticker');
             
             ws.onopen = function open() {
+                setConnect(true);
                 console.log('connected');
-                ws.send(JSON.stringify({ subscribe: ['AAPL'] }));
+                // clearTimeout(timeout);
+                ws.send(JSON.stringify({ subscribe: [ticker] }));
             };
         
             ws.onclose = function close() {
+                setConnect(false);
                 console.log('disconnected');
             };
         
             ws.onmessage = async function incoming(message) {
+                setMarketStatus('open');
+                clearTimeout(timeout);
 
                 const next = Yaticker?.decode(new Buffer(message.data, 'base64'));
                 setCurrentStock((current) => {
@@ -70,10 +88,16 @@ export function StockLive() {
             <Card
                 className="w-full"
                 decoration="top"
-                decorationColor="indigo"
-            >
-                <p className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">{currentStock && (currentStock as any).id}</p>
-                    <p className={`text-3xl font-semibold ${priceColor}`}>{currentStock && formatPrice((currentStock as any).price)}</p>
+                decorationColor={priceColor}
+                >
+                <div className='flex justify-between align-center'>
+                    <h1 className="text-tremor-content dark:text-dark-tremor-content text-3xl font-b">{ticker}</h1>
+                    <div className="flex-col text-right">
+                        {connect ? <p className="text-green-500">Connected</p> : <p className="text-slate-400">Connecting...</p>}
+                        {marketStatus === 'closed' ? (<p className="text-red-500">Market Closed</p>) : marketStatus === 'open' ? <p className="text-green-500">Market Open</p> : <p className="text-slate-400">Checking market status...</p>}
+                    </div>
+                </div>
+                <p className={`text-3xl font-semibold ${priceColor}`}>{currentStock && formatPrice((currentStock as any).price)}</p>
                 </Card>
             </div>
 
