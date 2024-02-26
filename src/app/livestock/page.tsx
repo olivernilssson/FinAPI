1e4db85e51dd5365c4c4525faa62cf17316e1d59
olivernilssson <olivernilsson@live.se>
2024-02-26 23:39:45 +0100

    Add RiFireFill icon to ticker display

--- src/app/livestock/page.tsx
+++ src/app/livestock/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { StockLive } from "@/lib/util/stock-live/stock-live"
 import { TextInput, Button } from "@tremor/react";
-import { RiSearchLine } from '@remixicon/react';
+import { RiSearchLine, RiFireFill } from '@remixicon/react';
 
 export default function LiveStock() {
     const [ticker, setTicker] = useState('');
@@ -24,7 +24,11 @@ export default function LiveStock() {
                 <Button onClick={() => setTickers([])} disabled={clearDisabled} color='red'>Clear Tickers</Button>
                 <Button onClick={addTicker} disabled={addDisabled} color='green'>Add Ticker</Button>
             </div>
-            <h1 className="text-tremor-content dark:text-dark-tremor-content text-3xl font-b">{ticker}</h1>
+            {ticker.length > 0 &&
+                <h1 className="flex text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-yellow-200 to-orange-500">
+                    {ticker}
+                    <RiFireFill className="w-10 h-10 animate-pulse text-orange-500" />
+                </h1>}
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
                 {tickers.map((ticker, index) => (
                     <StockLive key={index} ticker={ticker} />
